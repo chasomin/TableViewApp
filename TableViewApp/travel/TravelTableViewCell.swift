6fe178f8e2c13ab3092151ef314c4d9eba299eef
chasomin <somin49@naver.com>
2024-01-10 09:42:17 +0900

    테이블뷰컨트롤러 cellForRowAt 코드 정리

--- TableViewApp/travel/TravelTableViewCell.swift
+++ TableViewApp/travel/TravelTableViewCell.swift
@@ -16,4 +16,40 @@ class TravelTableViewCell: UITableViewCell {
     @IBOutlet var subtitleLabel: UILabel!
     
     @IBOutlet var dateLabel: UILabel!
+    
+    let format = DateFormatter()
+    let formatTostring = DateFormatter()
+
+    override func awakeFromNib() {
+        magazineImageView.contentMode = .scaleAspectFill
+        magazineImageView.layer.cornerRadius = 10
+        
+        titleLabel.font = .boldSystemFont(ofSize: 21)
+        titleLabel.numberOfLines = 0
+        
+        subtitleLabel.font = .boldSystemFont(ofSize: 15)
+        subtitleLabel.textColor = .lightGray
+
+        
+        format.dateFormat = "yyMMdd"
+        formatTostring.dateFormat = "yy년 MM월 dd일"
+        
+        dateLabel.font = .boldSystemFont(ofSize: 13)
+        dateLabel.textColor = .lightGray
+    }
+    
+    func configureCell(data: Magazine) {
+        magazineImageView.kf.setImage(with: URL(string: data.photo_image))
+        
+        titleLabel.text = data.title
+        
+        subtitleLabel.text = data.subtitle
+        
+        let date = format.date(from: data.date) ?? Date()
+        let result = formatTostring.string(from: date)
+        dateLabel.text = result
+
+        
+        
+    }
 }
--- TableViewApp/travel/TravelTableViewController.swift
+++ TableViewApp/travel/TravelTableViewController.swift
@@ -26,37 +26,11 @@ class TravelTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
-        
-        let url = URL(string: magazine[indexPath.row].photo_image)!
-        cell.magazineImageView.kf.setImage(with: url)
-        cell.magazineImageView.contentMode = .scaleAspectFill
-        cell.magazineImageView.layer.cornerRadius = 10
-        
-        cell.titleLabel.text = magazine[indexPath.row].title
-        cell.titleLabel.font = .boldSystemFont(ofSize: 21)
-        cell.titleLabel.numberOfLines = 0
-        
-        cell.subtitleLabel.text = magazine[indexPath.row].subtitle
-        cell.subtitleLabel.font = .boldSystemFont(ofSize: 15)
-        cell.subtitleLabel.textColor = .lightGray
-        
-        let format = DateFormatter()
-        format.dateFormat = "yyMMdd"
-        let date = format.date(from: magazine[indexPath.row].date) ?? Date()
-        
-        let formatTostring = DateFormatter()
-        formatTostring.dateFormat = "yy년 MM월 dd일"
-        let result = formatTostring.string(from: date)
 
-        cell.dateLabel.text = result
-        cell.dateLabel.font = .boldSystemFont(ofSize: 13)
-        cell.dateLabel.textColor = .lightGray
-
+        cell.configureCell(data: magazine[indexPath.row])
+        
         return cell
-        
     }
-    
-
 
 }
 
